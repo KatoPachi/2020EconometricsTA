bef02e1a89feef310f845777edc5f31cd7eeaabe
KatoPachi <vge008kh@student.econ.osaka-u.ac.jp>
2020-10-19 22:51:58 +0900

    add TA handouts

---
+++ II/_render.r
@@ -0,0 +1,23 @@
+#Render
+
+#'
+#+ library and wd
+library(rmarkdown)
+Sys.setenv(RSTUDIO_PANDOC = "C:/Users/katoo/AppData/Local/Pandoc")
+options(repo = "https://cran.rstudio.com/")
+
+#' from Rmd to pdf
+rmarkdown::render(
+  input = "II/201009/handout.rmd",
+  output_file = "handout.pdf",
+  output_dir = "II/201009", #いじらない
+  clean = TRUE, #いじらない
+  encoding = "utf8" #いじらない
+)
+
+#' from Rmd to R
+knitr::purl(
+    input = "II/201009/handout.rmd",
+    output = "R/#1.r",  
+    documentation = 1
+)
