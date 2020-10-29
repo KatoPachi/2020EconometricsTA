8907ef772276e6c7d2c8e9b9087d00c5091d9ebd
KatoPachi <vge008kh@student.econ.osaka-u.ac.jp>
2020-10-29 23:02:44 +0900

    purl - #1

--- R/script/TASession_3.r
+++ R/script/TASession_3.r
@@ -8,7 +8,7 @@ dt <- subset(dt, !is.na(survived)&!is.na(age)&!is.na(fare)&!is.na(female))
 dt <- dt[,c("survived", "age", "fare", "female")]
 
 set.seed(120511)
-train_id <- sample(1:nrow(dt), size = 2*nrow(dt)/3, replace = FALSE)
+train_id <- sample(1:nrow(dt), size = (2/3)*nrow(dt), replace = FALSE)
 train_dt <- dt[train_id,]
 test_dt <- dt[-train_id,]
 
@@ -150,13 +150,25 @@ stargazer(
     c("Log-Likelihood", round(LL_probit, 3), round(LL_logit, 3))),
   title = "Results of Probit and Logit model",
   label = "probit_logit",
-  type = "text", header = FALSE, font.size = "small",
+  type = "latex", header = FALSE, font.size = "small",
   table.placement = "h", omit.table.layout = "n"
 )
 
 
 
 
+
+
+## ----calib_probit_logit-------------------------------------------------------
+#probit
+cval_p <- b_probit[1] + 30*b_probit[3] + 37*b_probit[4] 
+female_p <- pnorm(cval_probit + b_probit[2]) - pnorm(cval_probit)
+#logit
+cval_l <- b_logit[1] + 30*b_logit[3] + 37*b_logit[4]
+female_l <- 1/(1 + exp(-(cval_l + b_logit[2]))) - 1/(1 + exp(-cval_l)) 
+# result
+print("Probit: Diff of prob. b/w average female and male"); female_p
+print("Logit: Diff of prob. b/w average female and male"); female_l
 
 
 ## ----pcp----------------------------------------------------------------------
@@ -181,9 +193,9 @@ out_Y <- test_dt$survived
 test_dt$"(Intercept)" <- 1
 out_X <- as.matrix(test_dt[,c("(Intercept)", "female", "age", "fare")])
 
-out_Xb_lpm <- X %*% matrix(coef(LPM), ncol = 1)
-out_Xb_probit <- X %*% matrix(b_probit, ncol = 1)
-out_Xb_logit <- X %*% matrix(b_logit, ncol = 1)
+out_Xb_lpm <- out_X %*% matrix(coef(LPM), ncol = 1)
+out_Xb_probit <- out_X %*% matrix(b_probit, ncol = 1)
+out_Xb_logit <- out_X %*% matrix(b_logit, ncol = 1)
 
 out_hatY_lpm <- ifelse(out_Xb_lpm > 0.5, 1, 0)
 out_hatY_probit <- ifelse(pnorm(out_Xb_probit) > 0.5, 1, 0)
@@ -222,138 +234,8 @@ stargazer(
     c("Pseudo R-squared", pr2_lpm, pr2_probit, pr2_logit)
   ),
   omit.table.layout = "n", table.placement = "t",
-  title = "Titanic Survivors: LPM, Probit (AME), and Logit (AME)",
+  title = "Titanic Survivors: LPM, Probit, and Logit",
   label = "titanic",
-  type = "text", header = FALSE
-)
-
-
-## ----data2--------------------------------------------------------------------
-house <- read.csv(file = "./data/housing.csv", header = TRUE,  sep = ",")
-house <- house[,c("Level", "lnPrice", "Top25")]
-house$Levelnum <- ifelse(
-  house$Level == 1, 25, 
-  ifelse(house$Level == 2, 50, 
-  ifelse(house$Level == 3, 75, 100)))
-head(house)
-
-
-## ----orderModel---------------------------------------------------------------
-library(MASS)
-library(tidyverse) #use case_when()
-
-ols <- lm(Levelnum ~ lnPrice + Top25, data = house)
-
-model <- factor(Level) ~ lnPrice + Top25
-oprobit <- polr(model, data = house, method = "probit")
-ologit <- polr(model, data = house, method = "logistic")
-
-a_oprobit <- round(oprobit$zeta, 3)
-a_ologit <- round(ologit$zeta, 3)
-
-xb_oprobit <- oprobit$lp 
-xb_ologit <- ologit$lp
-
-hatY_oprobit <- case_when(
-  xb_oprobit <= oprobit$zeta[1] ~ 1,
-  xb_oprobit <= oprobit$zeta[2] ~ 2,
-  xb_oprobit <= oprobit$zeta[3] ~ 3,
-  TRUE ~ 4
-)
-hatY_ologit <- case_when(
-  xb_ologit <= ologit$zeta[1] ~ 1,
-  xb_ologit <= ologit$zeta[2] ~ 2,
-  xb_ologit <= ologit$zeta[3] ~ 3,
-  TRUE ~ 4
-)
-
-pred_oprobit <- round(sum(house$Level == hatY_oprobit)/nrow(house), 3)
-pred_ologit <- round(sum(house$Level == hatY_ologit)/nrow(house), 3)
-
-
-## ----predict------------------------------------------------------------------
-quantef <- function(model) {
-  b <- coef(model)
-  val1 <- mean(house$lnPrice)*b[1] + b[2]
-  val0 <- mean(house$lnPrice)*b[1]
-
-  prob <- matrix(c(rep(val1, 3), rep(val0, 3)), ncol = 2, nrow = 3)
-  for (i in 1:3) {
-    for (j in 1:2) {
-      prob[i,j] <- pnorm(model$zeta[i] - prob[i,j])
-    }
-  }
-  Ey1 <- 25*prob[1,1] + 50*(prob[2,1]-prob[1,1]) + 
-    75*(prob[3,1]-prob[2,1]) + 100*(1-prob[3,1])
-  Ey0 <- 25*prob[1,2] + 50*(prob[2,2]-prob[1,2]) + 
-    75*(prob[3,2]-prob[2,2]) + 100*(1-prob[3,2])
-  
-  return(Ey1 - Ey0)
-} 
-
-ef_oprobit <- round(quantef(oprobit), 3)
-ef_ologit <- round(quantef(ologit), 3)
-
-
-## ----tab_orderModel, results = "asis"-----------------------------------------
-stargazer(
-  ols, oprobit, ologit,
-  report = "vcstp", keep.stat = c("n"),
-  omit = c("Constant"),
-  add.lines = list(
-    c("Cutoff value at 1|2", "", a_oprobit[1], a_ologit[1]),
-    c("Cutoff value at 2|3", "", a_oprobit[2], a_ologit[2]),
-    c("Cutoff value at 3|4", "", a_oprobit[3], a_ologit[3]),
-    c("Quantitative Effect of Top25", "", ef_oprobit, ef_ologit),
-    c("Percent correctly predicted", "", pred_oprobit, pred_ologit)
-  ),
-  omit.table.layout = "n", table.placement = "t",
-  title = "Floor Level of House: Ordered Probit and Logit Model",
-  label = "housing",
   type = "latex", header = FALSE
-)
-
-
-## ----supermarket--------------------------------------------------------------
-library(AER)
-data(BankWages)
-dt <- BankWages
-dt$job <- as.character(dt$job)
-dt$job <- factor(dt$job, levels = c("admin", "custodial", "manage"))
-head(BankWages, 5)
-
-
-## ----multinomial, results = "hide"--------------------------------------------
-library(nnet)
-est_mlogit <- multinom(job ~ education + gender, data = dt)
-
-# observations and percent correctly predicted
-pred <- est_mlogit$fitted.value
-pred <- colnames(pred)[apply(pred, 1, which.max)]
-n <- length(pred)
-pcp <- round(sum(pred == dt$job)/n, 3)
-
-# Log-likelihood and pseudo R-sq
-loglik1 <- as.numeric(nnet:::logLik.multinom(est_mlogit))
-est_mlogit0 <- multinom(job ~ 1, data = dt)
-loglik0 <- as.numeric(nnet:::logLik.multinom(est_mlogit0))
-pr2 <- round(1 - loglik1/loglik0, 3)
-
-
-## ----tab_multinomial, results = "asis"----------------------------------------
-stargazer(
-  est_mlogit,
-  covariate.labels = c("Education", "Female = 1"),
-  report = "vcstp", omit.stat = c("aic"),
-  add.lines = list(
-    c("Observations", n, ""),
-    c("Percent correctly predicted", pcp, ""),
-    c("Log-likelihood", round(loglik1, 3), ""),
-    c("Pseudo R-sq", pr2, "")
-  ),
-  omit.table.layout = "n", table.placement = "t",
-  title = "Multinomial Logit Model of Job Position",
-  label = "job",
-  type = "latex", header = FALSE  
 )
 
