619704e1b035281cd526860c1dd1e1b2476f16dd
KatoPachi <vge008kh@student.econ.osaka-u.ac.jp>
2020-12-20 15:33:29 +0900

    generate #8 file - #6

--- II/_render.r
+++ II/_render.r
@@ -3,21 +3,21 @@
 #'
 #+ library and wd
 library(rmarkdown)
-Sys.setenv(RSTUDIO_PANDOC = "C:/Users/katoo/AppData/Local/Pandoc")
+Sys.setenv(RSTUDIO_PANDOC = "C:/Program Files/Pandoc")
 options(repo = "https://cran.rstudio.com/")
 
 #' from Rmd to pdf
 rmarkdown::render(
-  input = "II/TAsession_5/handout.rmd",
+  input = "II/TAsession_8/handout.rmd",
   output_file = "handout.pdf",
-  output_dir = "II/TAsession_5", #いじらない
+  output_dir = "II/TAsession_8", #いじらない
   clean = TRUE, #いじらない
   encoding = "utf8" #いじらない
 )
 
 #' from Rmd to R
 knitr::purl(
-    input = "II/TAsession_5/handout.rmd",
+    input = "II/TAsession_6/handout.rmd",
     output = "R/script/TASession_5.r",  
     documentation = 1
 )
